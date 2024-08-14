e7421b93342a14534f193e360faa63e94bc551da
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-14 10:36:15 +0200

    Initial commit

---
+++ src/main/java/es/fercbrt/springbootcoursebackend/SpringbootCourseBackendApplication.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootcoursebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCourseBackendApplication.class, args);
+    }
+
+}
