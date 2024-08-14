2d4852f5d06dfc94a6559413834d61052534dc30
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-14 11:09:36 +0200

    Created the product entity

---
+++ src/main/java/es/fercbrt/springbootcoursebackend/entities/Product.java
@@ -0,0 +1,27 @@
+package es.fercbrt.springbootcoursebackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "description")
+    private String description;
+}
