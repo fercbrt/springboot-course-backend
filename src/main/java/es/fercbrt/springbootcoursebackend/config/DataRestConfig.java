0cf1cb5a8941ba1bb10dac93aef168106b9b2269
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-14 11:16:33 +0200

    Created a new configuration for the rest repository

---
+++ src/main/java/es/fercbrt/springbootcoursebackend/config/DataRestConfig.java
@@ -0,0 +1,16 @@
+package es.fercbrt.springbootcoursebackend.config;
+
+import es.fercbrt.springbootcoursebackend.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);
+    }
+}
